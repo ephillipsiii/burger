2917afad8cb8c3744b09cbe6fa43b000c58efc54
Lappy Mc Lap Face <ephillipsiii>
2018-05-20 16:40:18 -0600

    initial db creation, filled in values for burgers

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger_name, devoured)
+VALUES ('BBQ Bison Bacon Blue Cheese Cheddar Burger', false);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ('Elk Jalapeno Cheddar Burger', false);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ('American Classic Cheesburger', false);
