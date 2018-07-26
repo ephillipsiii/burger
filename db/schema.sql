10e3272bb563f0afe929a4ad235636f026367b5a
Edford Phillips <ed.d.phillips.iii@gmail.com>
2018-07-25 22:54:49 -0600

    attempting to connect to db and have db populate

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
@@ -7,4 +8,6 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
+SELECT * FROM burgers;
     
